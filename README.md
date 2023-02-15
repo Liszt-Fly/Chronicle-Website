aa8e5e5b4b40b8675098a05e78eba47dde725782
forestlet <hyf2021@outlook.com>
2023-02-15 22:36:59 +0800

    update

--- README.md
+++ README.md
@@ -3,7 +3,15 @@
 ## deploy 方法
 
 ```shell
+# 下载依赖
 pnpm run build
 
+# 运行
+pnpm run dev
+
+# 打包
+pnpm run build
+
+# 上传到服务器
 scp -r ./chronWebsite/ root@39.106.200.116:/projects
 ```
---
+++ src/components/Intro.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="blank h-16 s"> </div>
+    <div id="title" class="mt-10  h-40 flex flex-col items-center ">
+        <h1 class="sm:text-5xl font-semibold mb-6 text-2xl tracking-wide font-serif">The Future of Notes</h1>
+        <div id="description" class="w-120 text-neutral-900 text-center">
+            Chronicle is a note-taking application, but more precisely it is a highly ustomisable knowledge base.
+        </div>
+    </div>
+    <div class="text-center block mt-8">
+        <img src="http://mikes.oss-cn-beijing.aliyuncs.com/uPic/Feb-15-2023-16-54-53.gif"
+            class="w-full sm:w-7/12 text-center inline drop-shadow-lg">
+    </div>
+</template>

7e27d97a60274cd3f53d7889bf9429f5ebb7ca78
Liszt-Fly <1904231450@qq.com>
2023-02-15 17:16:12 +0800

    🎉 init: init this project

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,26 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import NavBar from './components/NavBar.vue';
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+	<!--# NavBar-->
+	<NavBar></NavBar>
+	<div class="blank h-16 s"> </div>
+	<div id="title" class="mt-10  h-40 flex flex-col items-center">
+		<h1 class="sm:text-5xl font-semibold mb-6 text-2xl tracking-wide">The Future of Notes</h1>
+		<div id="description" class=" hidden sm:block w-96 text-neutral-900 font-light text-center">Chronicle is a
+			note-taking
+			application, but
+			more
+			precisely it is a highly customisable knowledge base . </div>
+	</div>
+	<div class="text-center block mt-8">
+		<img src="http://mikes.oss-cn-beijing.aliyuncs.com/uPic/Feb-15-2023-16-54-53.gif"
+			class="w-full sm:w-7/12 text-center inline border-solid border-4">
+	</div>
+
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<style lang="scss" scoped>
+
 </style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup></script>
+<template>
+	<div
+		class=" border-solid  h-16  w-full flex items-center md:justify-evenly md:pl-16 md:pr-16 justify-between pl-4 pr-4 fixed z-10 bg-neutral-50">
+		<div class="logo font-extrabold text-3xl">Chronicle</div>
+		<div class="items md:flex justify-center flex-1 text-slate-800 font-semibold hidden ">
+			<div class="item mr-4 cursor-pointer ">Products</div>
+			<div class="item mr-4 cursor-pointer ">· &nbsp Solutions</div>
+			<div class="item mr-4 cursor-pointer ">· &nbsp Pricing</div>
+		</div>
+
+		<a class="btn-1 font-extrabold border-4 border-solid border-red-500" href="#">Download</a>
+	</div>
+</template>
+<style lang="scss" scoped>
+@import url("../assets/mixin.scss");
+</style>

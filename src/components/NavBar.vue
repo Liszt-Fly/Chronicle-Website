dd2393ae7e185cb78b6421063cc2b05d44d05348
Liszt-Fly <1904231450@qq.com>
2023-02-16 02:06:04 +0800

    ✨ feat: update css

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,9 +1,9 @@
 <script lang="ts" setup></script>
 <template>
 	<div
-		class=" border-solid  h-16  w-full flex items-center md:justify-evenly md:pl-16 md:pr-16 justify-between pl-4 pr-4 fixed z-10">
+		class=" border-solid  h-16  w-full flex items-center md:justify-evenly md:pl-16 md:pr-16 justify-between pl-4 pr-4 fixed z-10 border-b bg-white">
 
-		<div class="logo font-extrabold text-3xl font-serif cursor-pointer">Chronicle</div>
+		<div class="logo font-extrabold text-3xl  cursor-pointer">Chronicle</div>
 
 		<div class="items md:flex justify-center flex-1 text-slate-800 font-semibold hidden ">
 			<div class="item cursor-pointer hover:underline underline-offset-4 text-sm">Products</div>

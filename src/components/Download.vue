02dee91cc31e41cba5419594630633582e2d404e
Liszt-Fly <1904231450@qq.com>
2023-08-05 18:19:50 +0800

    更新图标 修改样式

--- src/components/Download.vue
+++ src/components/Download.vue
@@ -7,17 +7,18 @@
 			current version: beta 0.2.0
 		</div>
 		<div class="btns flex justify-around ">
-			<div class="download-btn cursor-pointer mac border-solid rounded-md bg-violet-500 text-slate-50 p-2 mr-10 w-80">
-				<i class="bi bi-apple mr-2"></i>
+			<div class="cursor-pointer mac border-solid rounded-md bg-gray-900  text-slate-50 p-4 mr-10  flex items-center">
+				<i class="bi bi-apple mr-2 text-[3rem]"></i>
 				<a href="https://github.com/mytho-team/mytho/releases/download/v0.2.0-alpha/mytho_0.2.0.dmg">
-					Download for Mac
+					<div class="font-extrabold text-[1.5rem]">macOS</div>
+					<div class="font-bold">Desktop App</div>
 				</a>
 			</div>
-			<div
-				class="download-btn mr-2 cursor-pointer mac border-solid rounded-md bg-violet-500 text-slate-50 p-1 pl-3 pr-3 w-80">
-				<i class="bi bi-windows"></i>
+			<div class=" mr-2 cursor-pointer mac border-solid rounded-md bg-gray-900 text-slate-50 p-4 flex  items-center">
+				<i class="bi bi-windows text-[2.8rem] mr-[0.8rem]"></i>
 				<a href="https://github.com/mytho-team/mytho/releases/download/v0.2.0-alpha/mytho_0.2.0.exe">
-					Donwload for Windows
+					<div class="font-extrabold text-[1.5rem]">windows</div>
+					<div class="font-bold">Desktop App</div>
 				</a>
 			</div>
 
